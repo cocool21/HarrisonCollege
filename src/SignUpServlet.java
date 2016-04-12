b798eded2a1e03dd8832ebf7175612ac7fed70b1
Sheldon Gonsalves <sheldongonsalves02@gmail.com>
2016-04-12 14:44:24 -0400

    added files for login

--- src/SignUpServlet.java
+++ src/SignUpServlet.java
@@ -14,28 +14,26 @@ import javax.servlet.http.HttpServletResponse;
 public class SignUpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public SignUpServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request,response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String name=request.getParameter("name");
+		String user_name =request.getParameter("newusername");
+		String password = request.getParameter("newpassword");
+		
+		dblogin db =new dblogin();
+		db.insertNewUser(name,user_name,password);
+		
 	}
 
 }
--- src/dblogin.java
+++ src/dblogin.java
@@ -5,6 +5,8 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 
 import customTools.DBUtil;
+import model.HcInstructor;
+import model.HcStudent;
 import model.HcUser;
 public class dblogin {
 
@@ -19,6 +21,66 @@ public class dblogin {
 				.setParameter("username",user_name)
 				.setParameter("password", password);
 		return query ;
+		
+	}
+	public void insertNewUser(String name ,String user_name ,String password)
+	{
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em1.getTransaction();
+		String user_type="student";
+		HcUser hcu = new HcUser();
+		hcu.setHcType(user_type);
+		hcu.setPassword(password);
+		hcu.setUsername(user_name);
+		System.out.println(user_name);
+		
+		hcu.setUserid(getNextUserid()+1);
+		trans.begin();
+		try
+		{
+			System.out.println("im here 2");
+			em1.persist(hcu);
+			em1.flush();
+			trans.commit();
+		}
+		catch(Exception e)
+		{
+			
+			trans.rollback();
+		}
+		finally
+		{
+			em1.close();
+		}
+		
+		
+	}
+	
+	protected long getNextUserid()
+	{
+		
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		TypedQuery<Long> query =em1.createQuery("SELECT max(h.userid) FROM HcUser h",Long.class);
+		
+		return (long) query.getSingleResult();
+	}
+	
+	public long getStudentID(long userid)
+	{
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		TypedQuery query =em1.createQuery("SELECT h.studentid FROM HcStudent h where h.hcUser.userid=:user_id",  HcStudent.class)
+				.setParameter("user_id", userid);
+		System.out.println((long)query.getSingleResult());
+		return (long)query.getSingleResult();
+	}
+	public long getTeacherID(long userid)
+	{
+		EntityManager em1=DBUtil.getEmFactory().createEntityManager();
+		TypedQuery query =em1.createQuery("SELECT h.instructorid FROM HcInstructor h where h.hcUser.userid=:user_id",HcInstructor.class)
+				.setParameter("user_id", userid);
+		System.out.println((long)query.getSingleResult());
+		return (long)query.getSingleResult();
+		
 		
 	}
 
